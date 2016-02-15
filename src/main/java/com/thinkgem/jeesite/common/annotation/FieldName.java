9c1af8896e02b96e6d818c8fea0c73cf1f3d27b8
vince <zhengweizzw@163.com>
2016-02-15 14:30:00 +0800

    首次注释测试提交

--- src/main/java/com/thinkgem/jeesite/common/annotation/FieldName.java
+++ src/main/java/com/thinkgem/jeesite/common/annotation/FieldName.java
@@ -6,7 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * bean中文名注解
+ * 自定义一个注解@FieldName，表示bean中文名注解
+ *
+ * 详解：【@Target】注解可以修饰的类型，此处为可修饰方法
+ * 		 【@Retention】注解的生命周期，此处为编译时有效
+ *		 【@interface】自定义注解
  */
 @Target(ElementType.METHOD)  
 @Retention(RetentionPolicy.RUNTIME)  
